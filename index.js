c5840652c139dd0d12da86a3aba64adbd8f27e83
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-03-17 12:44:09 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown() {
+  for (let i = 0; i >= 0; i--) {
+    console.log(10);
+    console.log(9)
+    console.log(8)
+    console.log(7)
+    console.log(6)
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+    console.log(0)
+  }
+}
